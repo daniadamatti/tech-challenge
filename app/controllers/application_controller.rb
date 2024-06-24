c1042e9f13e42ae7017b8204f00b2847f5eb71df
Daniela Adamatti <adamatti.daniela@gmail.com>
2024-06-24 13:13:29 -0300

    add session log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  before_action :current_cart
+
+  private
+
+  def current_cart
+    @cart ||= Cart.find_or_create_by(id: session[:cart_id]) do |cart|
+      cart.total_price = 0
+    end
+    session[:cart_id] = @cart.id
+  end
 end
